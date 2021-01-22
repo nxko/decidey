b7733f70410dda43afdd61b381b3be92b8c7948a
Nikolas Kottre <nikolas.kottre@hotelery.de>
2021-01-22 11:02:36 +0100

    Extracted StyledComponents from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,135 +1,26 @@
 import React, { MouseEvent, useRef } from 'react';
 import ReactTooltip from 'react-tooltip';
-import styled from 'styled-components';
 import DecisionCard from './components/decision-card';
 import OptionPill from './components/option-pill';
 import DecideyLogo from './images/decidey-logo.png';
-
-
-
-const Container = styled.div`
-background: #B644F2;
-  min-height: 100vh;
-  min-width: 100vw;
-  position: absolute;
-  top: 0;
-  left: 0;
-  text-align: center;
-  color: #ffffff;
-`;
-
-const ContentContainer = styled.div`
-  max-width: 500px;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const Headline = styled.h1`
-  font-size: 100px;
-`;
-
-const Paragraph = styled.p`
-  font-family: 'futura'; 
-`;
-
-const ButtonContainer = styled.div`
-  margin-top: 16px;
-`;
-
-const Button = styled.button`
-  background-color: transparent;
-  border: 1px solid white;
-  padding: 8px;
-  color: white;
-  font-family: 'futura';
-  margin-left: 4.5px;
-  margin-right: 4.5px;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  border-radius: 25px;
-  &:hover {
-    color: #B644F2;
-    background-color: white; 
-  }
-  &:focus{
-    outline: none;
-  }
-`;
-
-const InputField = styled.input`
-  border: none; 
-  border-radius: 20px;
-  height: 30px;
-  width: 250px; 
-  padding-left: 15px;
-  padding-right: 15px;
-  font-family: 'futura';
-  color:  #B644F2;
-  &:focus{
-    outline: none;
-  }
-`;
-
-const CardContainer = styled.div`
-  display: inline-flex; 
-  justify-content: center;
-`;
-
-const CheckboxContainer = styled.div`
-  display: inline-flex; 
-  justify-content: center;
-  align-items: center; 
-`;
-
-const CheckBoxWrapper = styled.div`
-  position: relative;
-`;
-const CheckBoxLabel = styled.label`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 42px;
-  height: 26px;
-  border-radius: 15px;
-  background: #efefef;
-  cursor: pointer;
-  &::after {
-    content: "";
-    display: block;
-    border-radius: 50%;
-    width: 18px;
-    height: 18px;
-    margin: 3px;
-    background: #ffffff;
-    box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
-    transition: 0.2s;
-  }
-`;
-const CheckBox = styled.input`
-  opacity: 0;
-  z-index: 1;
-  border-radius: 15px;
-  width: 42px;
-  height: 26px;
-  &:checked + ${CheckBoxLabel} {
-    background: #c07de3;
-    &::after {
-      content: "";
-      display: block;
-      border-radius: 50%;
-      width: 18px;
-      height: 18px;
-      margin-left: 21px;
-      transition: 0.2s;
-    }
-  }
-`;
+import { 
+  Button, 
+  ButtonContainer, 
+  CheckBox, 
+  CheckboxContainer, 
+  CheckBoxLabel, 
+  CheckBoxWrapper, 
+  Container, 
+  ContentContainer, 
+  InputField, 
+  Paragraph, 
+  CardContainer 
+} from './components';
 
 function App() {
   const [options, setOptions] = React.useState<string[]>([]);
   const [decisions, setDecisions] = React.useState<string[]>([]);
   const [latestDecision, setLatestDecision] = React.useState<string>();
-  const [decideButtonVisible, setDecideButtonVisible] = React.useState<boolean>(true);
 
   const last5 = decisions.slice(0, 5);
   let inputRef = useRef<HTMLInputElement>(null);
@@ -209,7 +100,7 @@ function App() {
             </CardContainer>
           )))}
           <br/><br/>
-          {options.length > 0 && decideButtonVisible && (
+          {options.length > 0 && (
             <Button onClick={makeDecision}>Entscheiden</Button>
           )}
           <Paragraph>
---
+++ src/components.ts
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+background: #B644F2;
+  min-height: 100vh;
+  min-width: 100vw;
+  position: absolute;
+  top: 0;
+  left: 0;
+  text-align: center;
+  color: #ffffff;
+`;
+
+export const ContentContainer = styled.div`
+  max-width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Headline = styled.h1`
+  font-size: 100px;
+`;
+
+export const Paragraph = styled.p`
+  font-family: 'futura'; 
+`;
+
+export const ButtonContainer = styled.div`
+  margin-top: 16px;
+`;
+
+export const Button = styled.button`
+  background-color: transparent;
+  border: 1px solid white;
+  padding: 8px;
+  color: white;
+  font-family: 'futura';
+  margin-left: 4.5px;
+  margin-right: 4.5px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  border-radius: 25px;
+  &:hover {
+    color: #B644F2;
+    background-color: white; 
+  }
+  &:focus{
+    outline: none;
+  }
+`;
+
+export const InputField = styled.input`
+  border: none; 
+  border-radius: 20px;
+  height: 30px;
+  width: 250px; 
+  padding-left: 15px;
+  padding-right: 15px;
+  font-family: 'futura';
+  color:  #B644F2;
+  &:focus{
+    outline: none;
+  }
+`;
+
+export const CardContainer = styled.div`
+  display: inline-flex; 
+  justify-content: center;
+`;
+
+export const CheckboxContainer = styled.div`
+  display: inline-flex; 
+  justify-content: center;
+  align-items: center; 
+`;
+
+export const CheckBoxWrapper = styled.div`
+  position: relative;
+`;
+export const CheckBoxLabel = styled.label`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 42px;
+  height: 26px;
+  border-radius: 15px;
+  background: #efefef;
+  cursor: pointer;
+  &::after {
+    content: "";
+    display: block;
+    border-radius: 50%;
+    width: 18px;
+    height: 18px;
+    margin: 3px;
+    background: #ffffff;
+    box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
+    transition: 0.2s;
+  }
+`;
+export const CheckBox = styled.input`
+  opacity: 0;
+  z-index: 1;
+  border-radius: 15px;
+  width: 42px;
+  height: 26px;
+  &:checked + ${CheckBoxLabel} {
+    background: #c07de3;
+    &::after {
+      content: "";
+      display: block;
+      border-radius: 50%;
+      width: 18px;
+      height: 18px;
+      margin-left: 21px;
+      transition: 0.2s;
+    }
+  }
+`;
